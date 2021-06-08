b7aa7eb5750d88e17cd126c961d7255de3863d63
hadzija <hadzijasale@gmail.com>
2021-06-08 11:45:18 +0200

    fix: Get user from session

--- delivery/http/post_handler.go
+++ delivery/http/post_handler.go
@@ -2,18 +2,22 @@ package http
 
 import (
 	"bytes"
+	json2 "encoding/json"
 	"github.com/APIGateway/globals"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 type PostHandler struct {
 	ContentURL string
+	UsersURL string
 }
 
 func NewPostHandler() *PostHandler {
-	return &PostHandler{ContentURL: globals.GetContentMicroserviceUrl()}
+	return &PostHandler{ContentURL: globals.GetContentMicroserviceUrl(), UsersURL: globals.GetUsersMicroserviceUrl()}
 }
 
 func (handler *PostHandler) GetAll(w http.ResponseWriter, r *http.Request) {
@@ -28,8 +32,33 @@ func (handler *PostHandler) GetAll(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
+func (handler *PostHandler) getUserIdFromSession(token string) (uint64, error){
+	requestURI := handler.UsersURL + "/api/userId"
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", requestURI, nil)
+	req.Header.Set("Authorization", token)
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+
+	var UserId uint64
+	json2.NewDecoder(resp.Body).Decode(&UserId)
+	log.Print("JSON Data: ")
+	log.Println(UserId)
+
+	return UserId, nil
+}
+
 func (handler *PostHandler) LikePost(w http.ResponseWriter, r *http.Request) {
-	requestURI := handler.ContentURL + "/api/post/like"
+	bearToken := r.Header.Get("Authorization")
+	userId, err := handler.getUserIdFromSession(bearToken)
+
+	params := url.Values{}
+	params.Add("userId", strconv.Itoa(int(userId)))
+
+	requestURI := handler.ContentURL + "/api/post/like?" + params.Encode()
 	body, bodyErr := ioutil.ReadAll(r.Body);
 	if bodyErr != nil {
 		log.Println(bodyErr)
@@ -37,6 +66,7 @@ func (handler *PostHandler) LikePost(w http.ResponseWriter, r *http.Request) {
 		return;
 	}
 	resp, err := http.Post(requestURI, "application/json", bytes.NewReader(body));
+	log.Println(resp)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
