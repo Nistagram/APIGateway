641739bdeffe36e01acfc2102ba98326174f5ff1
Gergo <49314980+gregVader@users.noreply.github.com>
2021-06-22 17:49:38 +0200

    feat: Feed posts implemented

---
+++ delivery/http/feed_handler.go
@@ -0,0 +1,40 @@
+package http
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/APIGateway/globals"
+)
+
+type FeedHandler struct {
+	ContentURL string
+	UsersURL   string
+}
+
+func NewFeedHandler() *FeedHandler {
+	return &FeedHandler{ContentURL: globals.GetContentMicroserviceUrl(), UsersURL: globals.GetUsersMicroserviceUrl()}
+}
+
+func (handler *FeedHandler) GetFeedPosts(w http.ResponseWriter, r *http.Request) {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", handler.UsersURL+"/api/follows/followings", nil)
+	req.Header.Set("Authorization", r.Header.Get("Authorization"))
+	resp, err := client.Do(req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	} else {
+		body, _ := ioutil.ReadAll(resp.Body)
+		req, _ := http.NewRequest("GET", handler.ContentURL+"/api/feed?page="+r.URL.Query().Get("page")+"&page_size="+r.URL.Query().Get("page_size"), bytes.NewReader(body))
+		resp, err := client.Do(req)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		} else {
+			writeResponse(&w, resp)
+		}
+	}
+}
